f826b55ab7cd6f525ac8b5abb7adf3bcf853882a
yunjipark0623 <bearji0623@naver.com>
2018-09-27 14:47:38 +0900

    180927

---
+++ 05/random.py
@@ -0,0 +1,13 @@
+import random
+
+numbers = []
+for num in range(0, 10) :
+    numbers.append(random.randrange(0, 10))
+
+print("생성된 리스트", numbers)
+
+for num in range(0, 10) :
+    if num not in numbers :
+        print("%d 숫자는 리스트에 없네요." %num)
+        
+##random.randrange는 랜덤으로 ()사이의 숫자를 생성해주는 함수
