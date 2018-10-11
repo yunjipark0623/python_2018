68e9b3cf1c8adc387c8f6782e8a189ecbdd9a1af
yunjipark0623 <bearji0623@naver.com>
2018-10-11 15:08:18 +0900

    181011

---
+++ 06/heart.py
@@ -0,0 +1,26 @@
+## 변수 선언 부분
+i, k, heartNum = 0, 0, 0
+numStr, ch, heartStr = "", "", ""
+
+##메인(main) 코드 부분
+numStr = input("숫자를 여러 개 입력하세요 : ")
+print("")
+
+i = 0
+ch = numStr[i]
+while True :
+    heartNum = int(ch)
+
+    heartStr = ""
+    for k in range (0, heartNum) :
+        heartStr += "\u2665"  ##\u2665 = 하트문자
+        k += 1
+
+    print(heartStr)
+
+    i += 1
+    if(i > len(numStr) - 1) :
+        break
+
+    ch = numStr[i]
+    
